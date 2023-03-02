f2dc7e1908f9841ada219340c726b0b6e6963cc9
jabison batista <jabison2013@hotmail.com>
2023-03-01 22:24:02 -0300

    first class: position

--- src/application/program.java
+++ src/application/program.java
@@ -1,10 +1,16 @@
 package application;
 
+
+import boardgame.position;
+
+//tokin(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)
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		position pos = new position(3, 5);
+			System.out.println(pos);
+		}
 	}
 
-}
+
